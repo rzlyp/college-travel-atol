f7413fced2303c6170db0408523315e4d271d964
firmannf <firmannizammudin@gmail.com>
2017-07-09 13:03:17 +0700

    ADD template dashboard

---
+++ resources/views/include/admin-sidebar.blade.php
@@ -0,0 +1,31 @@
+<div class="sidebar">
+    <div class="logo">
+        <img class="img img-responsive" width="148px" src="{{asset('img/im-logo-2x.png')}}" />
+    </div>
+
+    <ul class="nav">
+        <li>
+            <a href="#">
+                <i class="fa fa-dashboard"></i>
+                &nbsp;Dashboard
+            </a>
+        </li>
+        <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                <i class="fa fa-group"></i>
+                &nbsp;Data Petugas
+                <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu" role="menu">
+                <li><a href="#">Tambah Petugas</a></li>
+                <li><a href="#">Kelola Petugas</a></li>
+            </ul>
+        </li>
+        <li>
+            <a href="#">
+                <i class="fa fa-database"></i>
+                &nbsp;Data Transaksi
+            </a>
+        </li>
+    </ul>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/admin', function () {
+    return view('admin/dashboard');
+});
+
